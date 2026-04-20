a0b9a65c3ffa9dab2866d3d3c600ea4ad2b3329d
Mustafa Kemal <mustafakemalgocer@gmail.com>
2026-04-20 17:49:43 +0300

    Update get-spotify-token.js

--- get-spotify-token.js
+++ get-spotify-token.js
@@ -29,7 +29,7 @@ const { execSync } = require('child_process');
 // ║  BU DEĞERLERİ SPOTIFY DASHBOARD'DAN AL!     ║
 // ╚══════════════════════════════════════════════╝
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || 'd09effbb0a9d47a6a19f4fdd9c3cc314';
-const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || '0728555560844587844411b45799927c';
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || 'f4773b3e3cb2459282d41f612698af75';
 const REDIRECT_URI = 'http://127.0.0.1:8888/callback';
 const SCOPES = 'user-read-recently-played user-read-currently-playing';
 
